45cd2f1253915a8b299a57ead1b98634d0b56441
anchalgupta123 <ganchal22@gmail.com>
2019-12-18 23:01:55 -0800

    1st day work

---
+++ application/logs/log-2019-12-19.php
@@ -0,0 +1,90 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2019-12-19 07:13:37 --> Config Class Initialized
+INFO - 2019-12-19 07:13:37 --> Hooks Class Initialized
+DEBUG - 2019-12-19 07:13:37 --> UTF-8 Support Enabled
+INFO - 2019-12-19 07:13:37 --> Utf8 Class Initialized
+INFO - 2019-12-19 07:13:37 --> URI Class Initialized
+DEBUG - 2019-12-19 07:13:37 --> No URI present. Default controller set.
+INFO - 2019-12-19 07:13:37 --> Router Class Initialized
+INFO - 2019-12-19 07:13:37 --> Output Class Initialized
+INFO - 2019-12-19 07:13:37 --> Security Class Initialized
+DEBUG - 2019-12-19 07:13:37 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-12-19 07:13:37 --> Input Class Initialized
+INFO - 2019-12-19 07:13:37 --> Language Class Initialized
+INFO - 2019-12-19 07:13:37 --> Language Class Initialized
+INFO - 2019-12-19 07:13:37 --> Config Class Initialized
+INFO - 2019-12-19 07:13:37 --> Loader Class Initialized
+INFO - 2019-12-19 07:13:37 --> Controller Class Initialized
+DEBUG - 2019-12-19 07:13:37 --> Welcome MX_Controller Initialized
+DEBUG - 2019-12-19 07:13:37 --> File loaded: C:\xampp\htdocs\hmvc_folder\hmvc1\ci3-hmvc-master\application\modules/Welcome/views/welcome_message.php
+INFO - 2019-12-19 07:13:37 --> Final output sent to browser
+DEBUG - 2019-12-19 07:13:37 --> Total execution time: 0.9952
+INFO - 2019-12-19 07:14:31 --> Config Class Initialized
+INFO - 2019-12-19 07:14:31 --> Hooks Class Initialized
+DEBUG - 2019-12-19 07:14:31 --> UTF-8 Support Enabled
+INFO - 2019-12-19 07:14:31 --> Utf8 Class Initialized
+INFO - 2019-12-19 07:14:31 --> URI Class Initialized
+INFO - 2019-12-19 07:14:31 --> Router Class Initialized
+INFO - 2019-12-19 07:14:31 --> Output Class Initialized
+INFO - 2019-12-19 07:14:31 --> Security Class Initialized
+DEBUG - 2019-12-19 07:14:31 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-12-19 07:14:31 --> Input Class Initialized
+INFO - 2019-12-19 07:14:31 --> Language Class Initialized
+ERROR - 2019-12-19 07:14:31 --> 404 Page Not Found: /index
+INFO - 2019-12-19 07:14:39 --> Config Class Initialized
+INFO - 2019-12-19 07:14:39 --> Hooks Class Initialized
+DEBUG - 2019-12-19 07:14:39 --> UTF-8 Support Enabled
+INFO - 2019-12-19 07:14:39 --> Utf8 Class Initialized
+INFO - 2019-12-19 07:14:39 --> URI Class Initialized
+INFO - 2019-12-19 07:14:39 --> Router Class Initialized
+INFO - 2019-12-19 07:14:39 --> Output Class Initialized
+INFO - 2019-12-19 07:14:39 --> Security Class Initialized
+DEBUG - 2019-12-19 07:14:39 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-12-19 07:14:39 --> Input Class Initialized
+INFO - 2019-12-19 07:14:39 --> Language Class Initialized
+ERROR - 2019-12-19 07:14:39 --> 404 Page Not Found: ../modules/Home/controllers//index
+INFO - 2019-12-19 07:15:31 --> Config Class Initialized
+INFO - 2019-12-19 07:15:31 --> Hooks Class Initialized
+DEBUG - 2019-12-19 07:15:31 --> UTF-8 Support Enabled
+INFO - 2019-12-19 07:15:31 --> Utf8 Class Initialized
+INFO - 2019-12-19 07:15:31 --> URI Class Initialized
+INFO - 2019-12-19 07:15:31 --> Router Class Initialized
+INFO - 2019-12-19 07:15:31 --> Output Class Initialized
+INFO - 2019-12-19 07:15:31 --> Security Class Initialized
+DEBUG - 2019-12-19 07:15:31 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-12-19 07:15:31 --> Input Class Initialized
+INFO - 2019-12-19 07:15:31 --> Language Class Initialized
+ERROR - 2019-12-19 07:15:31 --> 404 Page Not Found: /index
+INFO - 2019-12-19 07:16:23 --> Config Class Initialized
+INFO - 2019-12-19 07:16:23 --> Hooks Class Initialized
+DEBUG - 2019-12-19 07:16:23 --> UTF-8 Support Enabled
+INFO - 2019-12-19 07:16:23 --> Utf8 Class Initialized
+INFO - 2019-12-19 07:16:23 --> URI Class Initialized
+DEBUG - 2019-12-19 07:16:23 --> No URI present. Default controller set.
+INFO - 2019-12-19 07:16:23 --> Router Class Initialized
+INFO - 2019-12-19 07:16:23 --> Output Class Initialized
+INFO - 2019-12-19 07:16:23 --> Security Class Initialized
+DEBUG - 2019-12-19 07:16:23 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-12-19 07:16:23 --> Input Class Initialized
+INFO - 2019-12-19 07:16:23 --> Language Class Initialized
+INFO - 2019-12-19 07:16:23 --> Language Class Initialized
+INFO - 2019-12-19 07:16:23 --> Config Class Initialized
+INFO - 2019-12-19 07:16:23 --> Loader Class Initialized
+INFO - 2019-12-19 07:16:23 --> Controller Class Initialized
+DEBUG - 2019-12-19 07:16:23 --> Welcome MX_Controller Initialized
+DEBUG - 2019-12-19 07:16:23 --> File loaded: C:\xampp\htdocs\hmvc_folder\hmvc1\ci3-hmvc-master\application\modules/Welcome/views/welcome_message.php
+INFO - 2019-12-19 07:16:23 --> Final output sent to browser
+DEBUG - 2019-12-19 07:16:23 --> Total execution time: 0.2585
+INFO - 2019-12-19 07:16:47 --> Config Class Initialized
+INFO - 2019-12-19 07:16:47 --> Hooks Class Initialized
+DEBUG - 2019-12-19 07:16:47 --> UTF-8 Support Enabled
+INFO - 2019-12-19 07:16:47 --> Utf8 Class Initialized
+INFO - 2019-12-19 07:16:47 --> URI Class Initialized
+INFO - 2019-12-19 07:16:47 --> Router Class Initialized
+INFO - 2019-12-19 07:16:47 --> Output Class Initialized
+INFO - 2019-12-19 07:16:47 --> Security Class Initialized
+DEBUG - 2019-12-19 07:16:47 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-12-19 07:16:47 --> Input Class Initialized
+INFO - 2019-12-19 07:16:47 --> Language Class Initialized
+ERROR - 2019-12-19 07:16:47 --> 404 Page Not Found: ../modules/Welcome/controllers//index
